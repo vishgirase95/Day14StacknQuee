b403f242daf74a9a5e7a4c1eee5ac97c7388a6ad
vish <vishgirase95@gmail.com>
2021-11-14 15:16:54 +0530

    [vishal] added push added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleApp19
+
+{
+    public class Node
+    {
+
+        public Node next;
+        public int data;
+
+        public Node(int x)
+        {
+            data = x;
+            next = null;
+
+
+        }
+
+    }
+
+
+    class Program
+    {
+        public Node Top = null;
+
+        public void push(int x)
+        {
+            Node node = new Node(x);
+
+            node.next = Top;
+            Top = node;
+
+
+        }
+
+
+        public void Display()
+        {
+
+            while (Top != null)
+            {
+                Console.WriteLine(Top.data);
+                Top = Top.next;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program obj = new Program();
+            obj.push(10);
+            obj.push(20);
+            obj.push(30);
+            obj.push(40);
+
+
+            obj.Display();
+
+        }
+    }
+}
